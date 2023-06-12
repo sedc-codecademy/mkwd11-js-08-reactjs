bdca2c6862df04258071a6d4577ca553fbf0d598
Gjorge Dimitrov <dimitrov.gjorge@protonmail.com>
2023-06-12 20:52:31 +0200

    class added

---
+++ G11/Class_03/lifecycle-react/src/App.jsx
@@ -0,0 +1,13 @@
+import LifeCycle from "./Components/Lifecycle_Demo/Lifecycle";
+import Products from "./Components/Products/Products";
+
+function App() {
+  return (
+    <>
+      {/* <LifeCycle title="A tittle for this component" /> */}
+      <Products />
+    </>
+  );
+}
+
+export default App;
---
+++ G11/Class_03/lifecycle-react/src/Components/Products/Products.jsx
@@ -0,0 +1,79 @@
+import { Component } from "react";
+import "./Products.css";
+import { ProductCard } from "../ProductCard/ProductCard";
+
+class Products extends Component {
+  URL = "https://fakestoreapi.com/products?limit=5";
+
+  products = [
+    {
+      id: 1,
+      title: "Product One",
+      description: "Product one description",
+      price: 100,
+      rating: 4,
+      image:
+        "https://www.coca-cola.com/content/dam/onexp/in/en/home-page-test-img/brands/fanta/Fanta-544x544.jpg",
+    },
+    {
+      id: 2,
+      title: "Product Two",
+      description: "Product Two description",
+      price: 99,
+      rating: 3.5,
+      image:
+        "https://www.coca-cola.com/content/dam/onexp/in/en/home-page-test-img/brands/fanta/Fanta-544x544.jpg",
+    },
+  ];
+
+  constructor() {
+    super();
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  handleFetchProducts = async () => {
+    //USING FETCH
+    // const response = await fetch(this.URL);
+    // const result = await response.json();
+
+    // console.log("RESULT: ", result);
+
+    // this.setState({
+    //   products: result,
+    // });
+
+    //USING OUR HARDCODE LIST BECAUSE OF CORS
+    this.setState({
+      products: this.products,
+    });
+  };
+
+  componentDidMount() {
+    this.handleFetchProducts();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Products Store</h1>
+
+        {this.state.products.length === 0 ? (
+          // Loading animation
+          <h3>Loading...</h3>
+        ) : (
+          <div className="products">
+            {this.state.products.map((product) => (
+              // Render the whole card
+              <ProductCard key={product.id} productProps={product} />
+            ))}
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+export default Products;
