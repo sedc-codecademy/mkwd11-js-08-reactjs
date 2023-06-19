570d8346b6245a2322101097cce43a4d15837df4
Gjorge Dimitrov <dimitrov.gjorge@protonmail.com>
2023-06-19 18:34:53 +0200

    class part 1 added

---
+++ G11/Class_05/my_app/src/Pages/About/About.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+    </div>
+  );
+};
---
+++ G11/Class_05/my_app/src/Pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  );
+};
